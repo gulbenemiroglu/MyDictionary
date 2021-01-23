b5c531e68dd89c4f7807eef8a6c21ea9581df025
Gülben Emiroğlu <gulben.emiroglu@gmail.com>
2021-01-23 20:51:39 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key,TValue value)
+        {
+            TKey[] tempKeys = keys;
+            TValue[] tempValues = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                //Console.WriteLine(keys[i]);
+            }
+
+            keys[keys.Length - 1] = key;
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+                //Console.WriteLine(values[i]);
+
+            }
+
+            values[values.Length - 1] = value;
+            
+            Console.WriteLine("ID :" + keys[keys.Length - 1] + "    Name :" + values[values.Length - 1]+"   listeye eklenmiştir!!!!!");
+            Console.WriteLine("");
+
+
+            Console.WriteLine("***********Yeni Liste***********");
+
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                
+                Console.WriteLine("ID :"+keys[i]+"    Name :"+values[i]);
+            }
+            Console.WriteLine("***********Liste Sonu***********");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> kisi = new MyDictionary<int, string>();
+            kisi.Add(1, "Gülben");
+            kisi.Add(2, "Bilge");
+            kisi.Add(3, "Erdem");
+            kisi.Add(4, "Gülşen");
+            kisi.Add(5, "Mustafa");
+            kisi.Add(6, "Merve");
+            kisi.Add(7, "Zeki");
+
+
+        }
+    }
+}
